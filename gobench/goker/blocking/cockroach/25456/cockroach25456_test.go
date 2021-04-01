785eacded6e0f244e0b929e9359adc0c7b2b1ae0
Nicolas Dilley <nicolasdilley@gmail.com>
2021-04-01 10:15:20 +0200

    Inlining struct aliasing via function call

--- gobench/goker/blocking/cockroach/25456/cockroach25456_test.go
+++ gobench/goker/blocking/cockroach/25456/cockroach25456_test.go
@@ -4,14 +4,46 @@ import (
 	"testing"
 )
 
-func process(quiescer chan struct{}) {
-	<-quiescer
+type Stopper struct {
+	quiescer chan struct{}
+}
+
+type Store struct {
+	stopper          *Stopper
+	consistencyQueue *consistencyQueue
+}
+
+type Replica struct {
+	store *Store
+}
+
+type consistencyQueue struct{}
+
+func (q *consistencyQueue) process(repl *Replica) {
+	<-repl.store.stopper.quiescer
+}
+
+type testContext struct {
+	store *Store
+	repl  *Replica
+}
+
+func (tc *testContext) StartWithStoreConfig(stopper *Stopper) {
+	if tc.store == nil {
+		tc.store = &Store{
+			consistencyQueue: &consistencyQueue{},
+		}
+	}
+	tc.store.stopper = stopper
+	tc.repl = &Replica{store: tc.store}
 }
 
 func TestCockroach25456(t *testing.T) {
-	quiescer := make(chan struct{})
+	stopper := &Stopper{quiescer: make(chan struct{})}
+	tc := testContext{}
+	tc.StartWithStoreConfig(stopper)
 
 	for i := 0; i < 2; i++ {
-		process(quiescer)
+		tc.store.consistencyQueue.process(tc.repl)
 	}
 }
