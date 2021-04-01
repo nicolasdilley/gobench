79cbdbd0982dae372fedb34cb7adaede25845a19
Nicolas Dilley <nicolasdilley@gmail.com>
2021-04-01 10:18:30 +0200

    Structs flattening

--- gobench/goker/blocking/cockroach/25456/cockroach25456_test.go
+++ gobench/goker/blocking/cockroach/25456/cockroach25456_test.go
@@ -1,43 +1,13 @@
 package cockroach25456
 
-import (
-	"testing"
-)
-
-type Stopper struct {
-	quiescer chan struct{}
+func process(quiescer chan struct{}) {
+	<-quiescer
 }
 
-type Store struct {
-	stopper          *Stopper
-	consistencyQueue *consistencyQueue
-}
-
-type Replica struct {
-	store *Store
-}
-
-type consistencyQueue struct{}
-
-func (q *consistencyQueue) process(repl *Replica) {
-	<-repl.store.stopper.quiescer
-}
-
-type testContext struct {
-	store *Store
-	repl  *Replica
-}
-
-func TestCockroach25456(t *testing.T) {
-	stopper := &Stopper{quiescer: make(chan struct{})}
-	tc := testContext{}
-	tc.store = &Store{
-		consistencyQueue: &consistencyQueue{},
-	}
-	tc.store.stopper = stopper
-	tc.repl = &Replica{store: tc.store}
+func testCockroach25456() {
+	quiescer := make(chan struct{})
 
 	for i := 0; i < 2; i++ {
-		tc.store.consistencyQueue.process(tc.repl)
+		process(quiescer)
 	}
 }
