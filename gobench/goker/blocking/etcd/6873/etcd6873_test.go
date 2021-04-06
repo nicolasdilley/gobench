0a191cbc6cdf66006a4b19c317de4e0082ef13f8
Nicolas Dilley <nicolasdilley@gmail.com>
2021-04-06 14:27:49 +0200

    Inlining channel creation

--- gobench/goker/blocking/etcd/6873/etcd6873_test.go
+++ gobench/goker/blocking/etcd/6873/etcd6873_test.go
@@ -20,20 +20,6 @@ type watchBroadcasts struct {
 	donec   chan struct{}
 }
 
-func newWatchBroadcasts() *watchBroadcasts {
-	wbs := &watchBroadcasts{
-		updatec: make(chan *watchBroadcast, 1),
-		donec:   make(chan struct{}),
-	}
-	go func() { // G2
-		defer close(wbs.donec)
-		for wb := range wbs.updatec {
-			wbs.coalesce(wb)
-		}
-	}()
-	return wbs
-}
-
 func (wbs *watchBroadcasts) coalesce(wb *watchBroadcast) {
 	wbs.mu.Lock()
 	wbs.mu.Unlock()
@@ -69,7 +55,16 @@ func (wbs *watchBroadcasts) update(wb *watchBroadcast) {
 ///---------------------G2,G3 deadlock-------------------------
 ///
 func TestEtcd(t *testing.T) {
-	wbs := newWatchBroadcasts() // G1
+	wbs := &watchBroadcasts{
+		updatec: make(chan *watchBroadcast, 1),
+		donec:   make(chan struct{}),
+	}
+	go func() { // G2
+		defer close(wbs.donec)
+		for wb := range wbs.updatec {
+			wbs.coalesce(wb)
+		}
+	}()
 	wbs.update(&watchBroadcast{})
 	go wbs.stop() // G3
 }
